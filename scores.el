729cb85e6db6bf9df98aab7464609f4f63e43429
erpreciso <trepreciso@gmail.com>
2023-11-25 09:37:43 +0100

    need to complete the link one

--- scores.el
+++ scores.el
@@ -9,3 +9,10 @@
   (:text Germany              :value +100 :type s)
   (:text WSJ                  :value +200 :type S)
   )
+ ("title-or-content"
+  (:text air show             :title-value +400 :content-value +100 :type s)
+  )
+ ("feed"
+  (:text VareseNews           :value -600 :type S :attr t :tags (t . sport))
+  (:text Il Post              :value +400 :type s :attr t)
+  )
