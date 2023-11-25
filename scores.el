966cbe3752b1b47ae1445ce6675d6da2d2702881
erpreciso <trepreciso@gmail.com>
2023-11-25 05:17:55 +0100

    First commit.
    Now refactoring to collect all headlines together and split the
    type in a property.

---
+++ scores.el
@@ -0,0 +1,9 @@
+;;; Elfeed score file     -*- lisp -*-
+(
+ ("tags"
+  (:tags (t .  science) :value +10 :type :attr )
+  (:tags (t .  sport) :value -500 :type :attr )
+  (:tags (t .  emacs) :value -100 :type :attr )
+  (:tags (t .  opinion) :value -50 :type :attr )
+  (:tags (t .  tuebingen) :value +250 :type :attr )
+  )
