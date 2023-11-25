1b4cfdb223b5a5e2587ade63b1c929d648edff33
erpreciso <trepreciso@gmail.com>
2023-11-25 07:29:55 +0100

    Create test version and implemented tag and title.

--- scores.el
+++ scores.el
@@ -1,9 +1,11 @@
 ;;; Elfeed score file     -*- lisp -*-
-(
- ("tags"
-  (:tags (t .  science) :value +10 :type :attr )
-  (:tags (t .  sport) :value -500 :type :attr )
-  (:tags (t .  emacs) :value -100 :type :attr )
-  (:tags (t .  opinion) :value -50 :type :attr )
-  (:tags (t .  tuebingen) :value +250 :type :attr )
+ ("tag"
+  (:tags (t . emacs     ) :value -100)
+  (:tags (t . sport     ) :value -500)
+  (:tags (t . science   ) :value +10)
+  )
+ ("title"
+  (:text macOS                :value -200 :type S)
+  (:text Germany              :value +100 :type s)
+  (:text WSJ                  :value +200 :type S)
   )
